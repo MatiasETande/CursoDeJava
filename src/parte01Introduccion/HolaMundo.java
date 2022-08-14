92a6917797a3c7a093de3b4d276c39e378960bf6
Matias Emanuel Tande <matiasetande@gmail.com>
2022-08-14 11:44:50 -0300

    Se crea el primer hola mundo

---
+++ src/parte01Introduccion/HolaMundo.java
@@ -0,0 +1,22 @@
+package parte01Introduccion;/*
+ *
+ *  Primer Hola mundo del Curso de Java - Lab de computacion TUP 2022
+ *  Tande,Matias Emanuel
+ *
+ * */
+
+
+
+public class HolaMundo {
+
+
+    public static void main (String[] args){
+
+        System.out.println ("Hola Mundo");
+
+
+    }
+
+
+
+}
