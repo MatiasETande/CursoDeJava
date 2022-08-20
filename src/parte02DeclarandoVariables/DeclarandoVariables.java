81b6f6ab8d57bccf5e8cb36f1123f7e7b4069bab
Matias Emanuel Tande <matiasetande@gmail.com>
2022-08-20 00:58:48 -0300

    Se agrega el programa Declarando Variables

---
+++ src/parte02DeclarandoVariables/DeclarandoVariables.java
@@ -0,0 +1,22 @@
+package parte02DeclarandoVariables;
+
+public class DeclarandoVariables {
+
+
+    public static void main(String[] args){
+
+
+        String nombre="Matias Emanuel Tande";
+
+        int edad=25;
+
+        float salario = 200.52f;
+
+        boolean carnet = true;
+
+        System.out.println("Soy "+nombre);
+
+    }
+
+
+}
