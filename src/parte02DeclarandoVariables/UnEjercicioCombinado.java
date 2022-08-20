75f599c1fedfcc00a4fe2a9ab8887f376741afee
Matias Emanuel Tande <matiasetande@gmail.com>
2022-08-20 01:39:46 -0300

    Se agrega programa Un ejercicio combinado

---
+++ src/parte02DeclarandoVariables/UnEjercicioCombinado.java
@@ -0,0 +1,22 @@
+package parte02DeclarandoVariables;
+
+public class UnEjercicioCombinado {
+
+    public static void main(String[] args){
+
+        float resultado;
+
+        resultado=(((float)6/5)-((float)2/3)*((float)7/2)+((float)2/30))/(((float)1/3)/5);
+
+
+        System.out.println(resultado);
+
+
+
+
+    }
+
+}
+
+
+//(((6/5)-(2/3)*((7/2))+(2/30))/((1/3)/5))
